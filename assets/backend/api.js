6dc0be88074ca71d39e84d57f2fb905229575fed
jbuzzanco <jcbuzzanco@gmail.com>
2016-12-26 14:00:09 -0500

    sets config to production url api & ajax url base
    ajax url base from store.host to config.apiOrigins
    as Antony suggested

--- assets/backend/api.js
+++ assets/backend/api.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const store = require('../scripts/store.js');
+const config = require('../scripts/config.js');
+
 
   const questionAppend = function (data) {
     return $.ajax({
-      url: store.host + '/surveys',
+      url: config.apiOrigins + '/surveys',
       method: 'POST',
       headers: {
         Authorization: 'Token token=' + store.user.token,
@@ -17,7 +19,7 @@ const store = require('../scripts/store.js');
 
   const deleteSurvey = function (_id) {
     return $.ajax({
-      url: store.host + '/surveys/' + _id,
+      url: config.apiOrigins + '/surveys/' + _id,
       method: 'DELETE',
       headers: {
         Authorization: 'Token token=' + store.user.token,
@@ -28,7 +30,7 @@ window.deleteSurvey = deleteSurvey;
 
 const updateImage = function (data) {
   return $.ajax({
-    url: store.host + '/images' + data.image.id,
+    url: config.apiOrigins + '/images' + data.image.id,
     method: 'PATCH',
     headers: {
       Authorization: 'Token token=' + store.user.token,
@@ -39,7 +41,7 @@ const updateImage = function (data) {
 
 const viewYourSurveys = function (data) {
   return $.ajax({
-    url: store.host + '/surveys',
+    url: config.apiOrigins + '/surveys',
     method: 'GET',
     headers: {
       Authorization: 'Token token=' + store.user.token,
@@ -50,7 +52,7 @@ const viewYourSurveys = function (data) {
 
 const viewAllSurveys = function (data) {
   return $.ajax({
-    url: store.host + '/surveys',
+    url: config.apiOrigins + '/surveys',
     method: 'GET',
     // headers: {
     //   Authorization: 'Token token=' + store.user.token,
--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -2,12 +2,13 @@
 
 // const config = require('../config.js');
 const store = require('../store.js');
+const config = require('../config.js');
 
 
 
 const signUp = (data) => {
   return $.ajax({
-    url: store.host + '/sign-up',
+    url: config.apiOrigins + '/sign-up',
     // url: config.apiOrigins.production + 'sign-up',
     method: 'POST',
     data,
@@ -16,7 +17,7 @@ const signUp = (data) => {
 
 const signIn = (data) => {
   return $.ajax({
-    url: store.host + '/sign-in',
+    url: config.apiOrigins + '/sign-in',
   // url: config.apiOrigins.production + 'sign-in',
     method: 'POST',
     data,
@@ -38,7 +39,7 @@ const signIn = (data) => {
 
 const changePassword = (data) => {
   return $.ajax({
-    url: store.host + '/change-password/' + store.user._id,
+    url: config.apiOrigins + '/change-password/' + store.user._id,
     // url: config.apiOrigins.production + 'change-password' + config.apiOrigins.production.user.id,
     method: 'PATCH',
     headers: {
@@ -51,7 +52,7 @@ const changePassword = (data) => {
 
   const signOut = () => {
     return $.ajax({
-      url: store.host + '/sign-out/' + store.user._id,
+      url: config.apiOrigins + '/sign-out/' + store.user._id,
       method: 'DELETE',
       headers: {
         Authorization: 'Token token=' + store.user.token,
